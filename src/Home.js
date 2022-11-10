131bbe63b50b5df0b90325ea5960362cd1ade448
Brittanie Joseph <brittanie.joseph03@gmail.com>
2022-11-09 22:30:29 -0500

    useEffect & dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,17 +8,27 @@ const Home = () => {
     { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3},
   ]);
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
     }
 
+    // useful for running code that needs to be run for every render
+    // runs twice here due to React.StrictMode running on App component
+    // pass dependencies (as a state) to useEffect (as an array) when you only want it to run in certain situations. if you only want it to run on the first render, pass an empty array
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);
+
   return (
     <div className="home">
         {/* blogs prop inline with component */}
         <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-        {/* blog filtering via props to return only mario's blogs */}
-        <BlogList blogs={ blogs.filter((blog) => blog.author === 'mario') } title="Mario's Blogs" />
+        <button onClick={() => setName('luigi')}>Change Name</button>
+        <p>{ name }</p>
     </div>
   );
 };
