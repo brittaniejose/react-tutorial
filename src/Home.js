c255720071b1a78d78d33061d4d742d14f45da19
Brittanie Joseph <brittanie.joseph03@gmail.com>
2022-11-09 21:38:48 -0500

    props, passing fns as props

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,26 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    // let name = 'mario';
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-    
-    const handleClick = () => {
-        setName('luigi');
-        setAge(30);
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3},
+  ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
-    }
+  return (
+    <div className="home">
+        {/* blogs prop inline with component */}
+        <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+        {/* blog filtering via props to return only mario's blogs */}
+        <BlogList blogs={ blogs.filter((blog) => blog.author === 'mario') } title="Mario's Blogs" />
+    </div>
+  );
+};
 
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
-
-            {/* passing an argument to a function inline an el */}
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
-        </div>
-     );
-}
- 
 export default Home;
